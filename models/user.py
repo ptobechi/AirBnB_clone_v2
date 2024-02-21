511fada8b980d1a37af8aeddf0b88a5183c152e6
Clevoh <clevelihasi.com>
2024-02-21 12:11:43 -0800

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-"""This is the user class"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """ Represent a user for a MySQL data.
-    """
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
 
-    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship("Place", backref="user", cascade="delete")
-        reviews = relationship("Review", backref="user", cascade="delete")
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    places = relationship('Place', cascade='all, delete', backref='user')
+    reviews = relationship('Review', cascade='all, delete', backref='user')
