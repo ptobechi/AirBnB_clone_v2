7e007cedab161486c0167fa955052dcc0fe40a8a
Clevoh <clevelihasi.com>
2024-02-21 11:46:12 -0800

    8. DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the city class"""
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """This is class for city
+    """
+
+    __tablename__ = "cities"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        state_id = ''
+        name = ''
--- models/user.py
+++ models/user.py
@@ -1,17 +1,24 @@
-!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+#!/usr/bin/python3
+"""This is the user class"""
+from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
+
+class User(BaseModel, Base):
+    """ Represent a user for a MySQL data.
+    """
+
     __tablename__ = "users"
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable = False)
-        password = Column(String(128), nullable = False)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
         first_name = Column(String(128))
         last_name = Column(String(128))
+        places = relationship("Place", backref="user", cascade="delete")
+        reviews = relationship("Review", backref="user", cascade="delete")
     else:
         email = ''
         password = ''
